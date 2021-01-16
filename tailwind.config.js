36e16039f030c3057cdef618e1130abde7cb8b71
Tomas Centeno <centenomas253@gmail.com>
2021-01-16 17:50:04 -0300

    Added css purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./**/*.html"],
+  },
   theme: {
     extend: {},
   },
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
 const path = require("path");
+const glob = require("glob");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+
+const PATHS = {
+  src: path.join(__dirname, "src"),
+};
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g);
+  }
+}
 
 module.exports = {
   mode: "development",
@@ -30,6 +44,19 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src/index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+    new PurgecssPlugin({
+      whitelist: ["body", ".whitelisted-class"],
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ["html", "js"],
+        },
+      ],
     }),
   ],
 };
